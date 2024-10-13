76bb9a166ffffb0e42374cbe3b2c1cf13d7078fb
tsthght <781181214@qq.com>
2024-10-13 21:22:02 +0800

    modify

--- og_full_sql/og_full_sql.c
+++ og_full_sql/og_full_sql.c
@@ -14,8 +14,6 @@
 #include "tcop/utility.h"
 
 static THR_LOCAL ExecutorRun_hook_type ExecutorRun_old_hook = NULL;
-static THR_LOCAL ExecutorEnd_hook_type ExecutorEnd_old_hook = NULL;
-
 static THR_LOCAL ProcessUtility_hook_type ProcessUtility_old_hook = NULL;
 static char *last_query = NULL;
 
@@ -41,11 +39,6 @@ static void ogExecutorRun_hook(QueryDesc* queryDesc, ScanDirection direction, lo
     ereport(LOG, (errmsg("[full sql]ExecutorRun_hook: %s, Transaction ID: %u", queryDesc->sourceText, xid)));
     standard_ExecutorRun(queryDesc, direction, count);
 }
-static void pgss_ExecutorFinish(QueryDesc *queryDesc) {
-    TransactionId xid = GetCurrentTransactionId();
-    ereport(LOG, (errmsg("[full sql]ExecutorEnd_hook: %s, Transaction ID: %u", queryDesc->sourceText, xid)));
-    standard_ExecutorEnd(queryDesc, direction, count);
-}
 
 
 void _PG_init(void);
@@ -56,9 +49,6 @@ void _PG_init(void) {
 
     ExecutorRun_old_hook = ExecutorRun_hook;
     ExecutorRun_hook = ogExecutorRun_hook;
-
-    ExecutorEnd_old_hook = ExecutorEnd_hook;
-    ExecutorEnd_hook = ogExecutorEnd_hook
 }
 
 PG_MODULE_MAGIC;
