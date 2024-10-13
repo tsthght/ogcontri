7a75c0585df39f92687430062398b760839c8d35
tsthght <781181214@qq.com>
2024-10-13 08:27:20 +0800

    og_full_sql

---
+++ og_full_sql/og_full_sql.c
@@ -0,0 +1,53 @@
+// clang-format off
+#include <postgres.h>
+#include <fmgr.h>
+// clang-format on
+#include "executor/spi.h"
+#include "executor/tstoreReceiver.h"
+#include "funcapi.h"
+#include "nodes/nodes.h"
+#include "utils/builtins.h"
+#include <miscadmin.h>
+#include "utils/timestamp.h"
+#include "postgres.h"
+#include "utils/elog.h"
+#include "tcop/utility.h"
+
+static THR_LOCAL ExecutorEnd_hook_type ExecutorEnd_old_hook = NULL;
+static THR_LOCAL ProcessUtility_hook_type ProcessUtility_old_hook = NULL;
+static char *last_query = NULL;
+
+static void ogProcessUtility_hook(Node* parsetree, const char* queryString, ParamListInfo params,
+    bool isTopLevel, DestReceiver* dest,
+#ifdef PGXC
+    bool sentToRemote,
+#endif /* PGXC */
+    char* completionTag,
+    bool isCTAS) {
+    Query *query = (Query *)parsetree;
+    // char *queryString = nodeToString(query);
+    ereport(LOG, (errmsg("[full sql]ProcessUtility_hook: %s, Transaction ID: %u", queryString,GetCurrentTransactionId())));
+    if (ProcessUtility_old_hook) {
+        ProcessUtility_old_hook(parsetree,queryString,params,isTopLevel,dest,sentToRemote,completionTag,isCTAS);
+    }
+}
+
+static void ogExecutorEnd_hook(QueryDesc *queryDesc) {
+    TransactionId xid = GetCurrentTransactionId();
+    ereport(LOG, (errmsg("[full sql]ExecutorEnd_hook: %s, Transaction ID: %u", queryDesc->sourceText, xid)));
+    if (ExecutorEnd_old_hook) {
+        ExecutorEnd_old_hook(queryDesc);
+    }
+}
+
+void _PG_init(void);
+
+void _PG_init(void) {
+    ProcessUtility_old_hook = ProcessUtility_hook;
+    ProcessUtility_hook = ogProcessUtility_hook;
+
+    ExecutorEnd_old_hook = ExecutorEnd_hook;
+    ExecutorEnd_hook = ogExecutorEnd_hook;
+}
+
+PG_MODULE_MAGIC;
